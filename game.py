0b17344317268c41c5ae11650538a7e11988e1f5
sloneearly <106164242+sloneearly@users.noreply.github.com>
2024-06-19 11:15:51 -0500

    made sprite attached to zimblort

--- game.py
+++ game.py
@@ -19,23 +19,7 @@ clock = pygame.time.Clock()
 sprites = pygame.sprite.Group()
 sprite_sheet_image = pygame.image.load('assets/doux.png').convert_alpha()
 sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
-
-animation_list = []
 animation_steps = [4, 6, 3, 4]
-action = 0 
-last_update = pygame.time.get_ticks()
-animation_cooldown = 50 #ms
-frame = 0
-step_counter = 0
-
-for anim in animation_steps:
-    temp_img_list = []
-    for _ in range(anim):
-        temp_img_list.append(sprite_sheet.get_image(step_counter, 24, 24, 3, BLACK))
-        step_counter += 1
-    animation_list.append(temp_img_list)
-
-
 
 if check_webcam():
     print("Webcam confirmed to exist")
@@ -52,35 +36,13 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    if watching_event.is_set():
-        new_action = 1
-    else:
-        new_action = 0
-
-
-    # reset frame if the action has changed
-    if new_action != action:
-        action = new_action
-        frame = 0
-
-    # update animation frame
-    current_time = pygame.time.get_ticks()
-    if current_time - last_update >= animation_cooldown:
-        frame += 1
-        last_update = current_time
-        if frame >= len(animation_list[action]):
-            frame = 0
-
-    # draw background
-    win.fill((12, 24, 36))
-
-    zimblort.draw(win)
+    # Update and draw Zimblort
+    win.fill((12, 24, 36))  # Draw background
     zimblort.update()
-
-    # draw current frame of animation
-    screen.blit(animation_list[action][frame], (zimblort.x, zimblort.y))
+    zimblort.draw(win)
 
     pygame.display.flip()
     clock.tick(120)
+
 
 pygame.quit()
--- zimblort.py
+++ zimblort.py
@@ -3,7 +3,9 @@ import pygame
 
 class Zimblort:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, sprite_sheet, animation_steps, animation_cooldown=50):
+
+        #zimblort object 
         self.x = int(x)
         self.y = int(y)
         self.rect = pygame.Rect((self.x, self.y, 32, 32))
@@ -12,15 +14,51 @@ class Zimblort:
         self.velY = 0
         self.speed = 0.3
 
+        #sprite and animation
+        self.sprite_sheet = sprite_sheet
+        self.animation_steps = animation_steps
+        self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.animation_cooldown = animation_cooldown
+        self.action = 0  # idle action
+        self.frame = 0
+        self.last_update = pygame.time.get_ticks()
+
     def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_update >= self.animation_cooldown:
+            self.frame += 1
+            self.last_update = current_time
+            if self.frame >= len(self.animation_list[self.action]):
+                self.frame = 0
+
+        image = self.animation_list[self.action][self.frame]
+        win.blit(image, (self.x, self.y))
 
     def update(self):
         self.velX = 0
         self.velY = 0
         if watching_event.is_set():
+            new_action = 1 
+        else:
+            new_action = 0  
+
+        if new_action != self.action:
+            self.action = new_action
+            self.frame = 0
+
+        if self.action == 1:
             self.velX = self.speed
 
         self.x += self.velX
-
         self.rect = pygame.Rect(int(self.x), int(self.y), 32, 32)
+
+    def load_images(self, sprite_sheet, animation_steps):
+        animation_list = []
+        step_counter = 0
+        for anim in animation_steps:
+            temp_img_list = []
+            for _ in range(anim):
+                temp_img_list.append(sprite_sheet.get_image(step_counter, 24, 24, 3, (0, 0, 0)))
+                step_counter += 1
+            animation_list.append(temp_img_list)
+        return animation_list
